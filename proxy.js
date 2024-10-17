4e13a0fb23202bd7913633231ff01311d79be5b0
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-10-17 10:29:17 +0530

    Update proxy.js to use new IP address for the server

--- proxy.js
+++ proxy.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.use('/', (req, res) => {
-  const url = 'http://192.168.137.1:2000' + req.url;
+  const url = 'http://100.105.87.81:2000' + req.url;
   req.pipe(request(url)).pipe(res);
 });
 
