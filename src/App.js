9e9020f95443a4b01ca54cfa1775b8d3b36eee5e
Bala Likhith Kanigolla <55309319+likhithkanigolla@users.noreply.github.com>
2025-02-10 22:00:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ function App() {
             setTimeout(() => {
                 setIsOn((prevState) => ({
                     ...prevState,
-                    valve1: true, // Close valve1
+                    valve1: false, // Prev(Close valve1) Now (Open valve1) 
                     valve2: false, // Open valve2
                     valve3: false,
                     valve4: true
